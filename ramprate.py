bb2b702d219cb5e901a94e42f78eaa97033a19d1
Christopher <ctr_287@sbcglobal.net>
2019-07-22 10:32:13 -0500

    initial work for ramp rate control

---
+++ ramprate.py
@@ -0,0 +1,22 @@
+import time
+class MagnetRamp():
+	def __init__(self):
+		self.lastTime = time.time() #time at which magnet was last updated
+		self.lastSP = 0 #Last set point that was sent to magnet
+		self.want_current = 0 #Current set by user
+		self.want_speed = 0 #*actually* in amps per hour
+		self.ramp_rate_out = 0 #Ramp rate to give to the magnet.
+
+	def update_magnet(self,current_now):
+		timedelta = time.time()-self.lastTime
+		magdelta = timedelta * self.want_speed/3600
+		#want speed is in amps per hour. Convert it to amps per second
+
+		if current_now < self.want_current:
+			nextSP = max(self.want_current,
+						 self.lastSP + magdelta)
+
+		elif current_now > self.want_current
+			nextSP = min(self.want_current,
+						 self.lastSP - magdelta)
+			
