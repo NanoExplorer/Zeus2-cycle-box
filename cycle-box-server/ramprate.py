236a3e9cbeac7cc0c2956bec3b52d511ab6b659b
Christopher Rooney <ctr_287@sbcglobal.net>
2022-08-25 15:27:24 -0400

    refactor magnet ramp helper

--- cycle-box-server/ramprate.py
+++ cycle-box-server/ramprate.py
@@ -1,41 +1,37 @@
 import time
 
+
 class MagnetRamp():
     def __init__(self):
-        self.lastTime = time.time() #time at which magnet was last updated
-        self.lastSP = 0 #Last set point that was sent to magnet
-        self.want_current = 0 #Current set by user
-        self.want_speed = 0 #*actually* in amps per hour
-        self.ramp_rate_out = 0 #Ramp rate to give to the magnet.
+        self.lastTime = time.time()  # time at which magnet was last updated
+        self.lastSP = 0  # Last set point that was sent to magnet
+        self.want_current = 0  # Current set by user
+        self.want_speed = 0  # *actually* in amps per hour
+        self.ramp_rate_out = 0  # Ramp rate to give to the magnet.
 
-    def update_magnet(self,current_now):
+    def update_magnet(self, current_now):
         timedelta = time.time()-self.lastTime
-        magdelta = timedelta * self.want_speed/3600
-        #want speed is in amps per hour. Convert it to amps per second
+        self.lastTime = time.time()
 
-        if current_now < self.want_current:
+        magdelta = timedelta * self.want_speed/3600
+        # want speed is in amps per hour. Convert it to amps per second
+
+        if self.lastSP < self.want_current:
             nextSP = max(self.want_current,
                          self.lastSP + magdelta)
 
-        elif current_now > self.want_current
+        elif self.lastSP > self.want_current:
             nextSP = min(self.want_current,
                          self.lastSP - magdelta)
 
-class SimpleRamp():
-    def __init__(self,current,speed):
-        self.start_time = time.time() #time at which magnet was started
-        self.start_current = current #Current set by user
-        self.want_speed = speed #*actually* in amps per hour
+        else:
+            nextSP = self.want_current
 
-    def update_magnet(self):
-        timedelta = time.time()-self.start_time
-        magdelta = timedelta * self.want_speed/3600
-        #want speed is in amps per hour. Convert it to amps per second
+        self.lastSP = nextSP
+        return nextSP
 
-        if current_now < self.want_current:
-            nextSP = max(self.want_current,
-                         self.lastSP + magdelta)
+    def set_want_speed_amps_per_hr(self, speed):
+        self.want_speed = speed
 
-        elif current_now > self.want_current
-            nextSP = min(self.want_current,
-                         self.lastSP - magdelta)
+    def set_want_speed_amps_per_min(self, speed):
+        self.want_speed = speed * 3600
